0c42dc86115170479fcf74efeccc02785dc077ad
Himanshu Ramteke <himanshuramteke007@gmail.com>
2026-02-24 18:32:31 +0530

    added gemini for AI-Jobs

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,10 +1,23 @@
 import { inngest } from "./client";
+import { gemini, createAgent } from "@inngest/agent-kit";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
-  { event: "test/hello.world" },
-  async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello World!` };
+  { event: "agent/hello" },
+  async () => {
+    const helloAgent = createAgent({
+      name: "hello-agent",
+      description: "A simple agent that say hello",
+      system: "You are a helpful assistant.Always greet with enthusiasm",
+      model: gemini({ model: "gemini-2.5-flash" }),
+    });
+
+    const { output } = await helloAgent.run("Say hello to user!");
+
+    const message = output[0].type === "text" ? output[0].content : "";
+
+    return {
+      message: message,
+    };
   },
 );
